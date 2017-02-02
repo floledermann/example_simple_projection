471ec530bd4dc4663912094dfc14014a51e77b81
Flo Ledermann <ledermann@ims.tuwien.ac.at>
2017-02-02 11:39:32 +0100

    improved support for capturing different methods of setting style properties

--- instrument.js
+++ instrument.js
@@ -3,7 +3,19 @@
 //"use strict";
 
 (function() {
+
+    var NAMES = {};
     
+    // source: https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Properties_Reference
+    NAMES.cssAttributes = ["background","backgroundAttachment","backgroundColor","backgroundImage","backgroundPosition","backgroundRepeat","border","borderBottom","borderBottomColor","borderBottomStyle","borderBottomWidth","borderColor","borderLeft","borderLeftColor","borderLeftStyle","borderLeftWidth","borderRight","borderRightColor","borderRightStyle","borderRightWidth","borderStyle","borderTop","borderTopColor","borderTopStyle","borderTopWidth","borderWidth","clear","clip","color","cursor","display","filter","float","font","fontFamily","fontSize","fontVariant","fontWeight","height","left","letterSpacing","lineHeight","listStyle","listStyleImage","listStylePosition","listStyleType","margin","marginBottom","marginLeft","marginRight","marginTop","overflow","padding","paddingBottom","paddingLeft","paddingRight","paddingTop","pageBreakAfter","pageBreakBefore","position","textAlign","textDecoration","textIndent","textTransform","transform","top","verticalAlign","visibility","width","zIndex"];
+    
+    // these seem not to be defined in Chrome: cssFloat 
+    // TODO: there are more css properties defined on the style object in Chrome, check in debugger!
+    
+    // source: https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D
+    NAMES.canvasContext2dMethods = ["addHitRegion","arc","arcTo","asyncDrawXULElement","beginPath","bezierCurveTo","clearHitRegions","clearRect","clip","closePath","createImageData","createLinearGradient","createPattern","createRadialGradient","drawFocusIfNeeded","drawImage","drawWidgetAsOnScreen","drawWindow","ellipse","fill","fillRect","fillText","getImageData","getLineDash","isPointInPath","isPointInStroke","lineTo","measureText","moveTo","putImageData","quadraticCurveTo","rect","removeHitRegion","resetTransform","restore","rotate","save","scale","scrollPathIntoView","setLineDash","setTransform","stroke","strokeRect","strokeText","transform","translate"
+    ];
+
     var filterLogPrefix = "";
     //filterLogPrefix = "HTMLCanvasElement.";
     var filterObject = function(obj) {
@@ -43,33 +55,44 @@
     
     function proxySetter(obj, propName, callback) {
         var descriptor = Object.getOwnPropertyDescriptor(obj, propName);
+        var originalFunction = null;
         if (!descriptor) {
             console.warn("Warning: no property descriptor for " + propName + " on " + obj);
         }
-        var originalFunction = descriptor.set;
-        if (!originalFunction) {
-            console.warn("Warning: no property named " + propName + " on " + obj);
+        else {
+            originalFunction = descriptor.set;
+            if (!originalFunction) {
+                // no setter previously defined
+                // we have a problem here: we cannot assign properties that have the same name
+                // as the setter (i.e. original property name)
+                // TODO: what to do? maybe cache value and define a getter to retrieve it?
+                // console.warn("Warning: no property named " + propName + " on " + obj);
+            }
         }
         Object.defineProperty(obj, propName, {
             set: function() {
-                callback.call(this, arguments, originalFunction);
+                callback.call(this, arguments, propName, originalFunction);
                 if (originalFunction) {
                     originalFunction.apply(this, arguments);
+                }
+                else {
+                    // this does not work, as we cannot set "raw" property of same name
+                    // as setter (infinite recursion)
+                    // obj[propName] = arguments[0];
                 }
             }
         })
     }
-    
-/*
-    proxyMethod(document, "createElement", function(tagName, options) {
-        log("creating element: " + tagName);
-    });
-*/   
+      
     function logFunction(namePrefix, func) {
         return function(arguments, originalFunction) {
-            var str = Array.prototype.join.call(arguments, ", ");
+            // surround strings in ""
+            var args = Array.prototype.map.call(arguments, arg => (typeof arg == "string") ? ('"' + arg + '"') : arg );
+            var str = args.join(", ");
             log(namePrefix + "(" + str + ")", this);
-            if (func) return func.call(this,arguments, originalFunction);
+            if (func) {
+                return func.call(this,arguments, originalFunction);
+            }
         }
     }
     
@@ -80,50 +103,29 @@
     function logAllSetters(obj, attributeNames, prefix, func) {
         attributeNames.forEach(name => proxySetter(obj, name, logFunction(prefix + name, func)));
     }
-
-    function proxyCanvas(canvas) {
-        // source: https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D
-        var canvasContext2dMethods = ["addHitRegion","arc","arcTo","asyncDrawXULElement","beginPath","bezierCurveTo","clearHitRegions","clearRect","clip","closePath","createImageData","createLinearGradient","createPattern","createRadialGradient","drawFocusIfNeeded","drawImage","drawWidgetAsOnScreen","drawWindow","ellipse","fill","fillRect","fillText","getImageData","getLineDash","isPointInPath","isPointInStroke","lineTo","measureText","moveTo","putImageData","quadraticCurveTo","rect","removeHitRegion","resetTransform","restore","rotate","save","scale","scrollPathIntoView","setLineDash","setTransform","stroke","strokeRect","strokeText","transform","translate"
-        ];
-        /*
-        {
-            "lineTo": logFunction("CanvasRenderingContext2D.lineTo"),
-            "lineTo": logFunction("CanvasRenderingContext2D.lineTo"),
-        }*/
-        
-        proxyMethod(canvas, "getContext", logFunction("HTMLCanvasElement.getContext", function(arguments, originalFunction) {
-            // contextType, contextAttributes
-            if (arguments[0] == "2d") {
-                if (this._proxyContext2d) return this._proxyContext2d;
-                var context = originalFunction.apply(this, arguments);
-                logAll(context, canvasContext2dMethods, "HTMLCanvasElement.CanvasRenderingContext2D.");
-                this._proxyContext2d = context;
-                return context;
-            }
-            else {
-                console.warn("HTMLCanvasElement.getContext: Canvas context type " + arguments[0] + " requested - proxy not implemented");
-            }
-        }));
-    }
     
     var documentMethods = {
         "createElement": function(arguments, originalFunction) {
             // arguments: tagName, options
             var tagName = arguments[0].toLowerCase();
-            log("creating element: " + tagName);
+            log("Document.createElement(" + tagName + ")");
             var el = originalFunction.apply(this, arguments);
             if (tagName == "canvas") {
                 proxyCanvas(el);
             }
             if (el.style) {
-                logAllSetters(el.style, cssAttributes, "CSSStyleDeclaration<set>");
+                logAllSetters(el.style, NAMES.cssAttributes, "CSSStyleDeclaration.<set>", function(arguments, propName, originalFunction) {
+                    // convert camelcase to dashed CSS attribute
+                    propName = propName.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
+                    this.setProperty(propName, arguments[0]);
+                });
             }
             return el;
             
         },
         "createElementNS": function(arguments, originalFunction) {
             // arguments: namespaceURI, qualifiedName, options
-            log("creating element: " + arguments[0] + ":" + arguments[1]);
+            log("Document.createElementNS(" + arguments[0] + "," + arguments[1] + ")");
         }
     };
     
@@ -140,15 +142,40 @@
     
     proxyMethod(Element.prototype, "setAttribute", function(arguments, originalFunction) {
         // arguments: name, value
-        log("setAttribute: " + arguments[0] + " to " + arguments[1] + " on " + this.tagName);
+        log("Element.setAttribute(" + arguments[0] + "," + arguments[1] + ") on " + this.tagName);
     });
 
     proxySetter(HTMLElement.prototype, "style", function(arguments, originalFunction) {
-        log("style set: \"" + arguments[0] + "\" on " + this.tagName);
+        log("Element.<set>style(\"" + arguments[0] + "\") on " + this.tagName);
     });
-
-    // source: https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Properties_Reference
-    var cssAttributes = ["background","backgroundAttachment","backgroundColor","backgroundImage","backgroundPosition","backgroundRepeat","border","borderBottom","borderBottomColor","borderBottomStyle","borderBottomWidth","borderColor","borderLeft","borderLeftColor","borderLeftStyle","borderLeftWidth","borderRight","borderRightColor","borderRightStyle","borderRightWidth","borderStyle","borderTop","borderTopColor","borderTopStyle","borderTopWidth","borderWidth","clear","clip","color","cursor","display","filter","font","fontFamily","fontSize","fontVariant","fontWeight","height","left","letterSpacing","lineHeight","listStyle","listStyleImage","listStylePosition","listStyleType","margin","marginBottom","marginLeft","marginRight","marginTop","overflow","padding","paddingBottom","paddingLeft","paddingRight","paddingTop","pageBreakAfter","pageBreakBefore","position","cssFloat","textAlign","textDecoration","textDecorationBlink","textDecorationLineThrough","textDecorationNone","textDecorationOverline","textDecorationUnderline","textIndent","textTransform","top","verticalAlign","visibility","width","zIndex"];
     
-    logAllSetters(CSSStyleDeclaration.prototype, cssAttributes, "CSSStyleDeclaration<set>");
+    //var cssStyleDeclarationSetters = [""];
+    
+    //logAllSetters(CSSStyleDeclaration.prototype, NAMES.cssAttributes, "CSSStyleDeclaration.<set>");
+    
+    logAll(CSSStyleDeclaration.prototype, ["setProperty", "removeProperty"], "CSSStyleDeclaration.")
+    logAllSetters(CSSStyleDeclaration.prototype, ["cssText"], "CSSStyleDeclaration.<set>")
+    
+    function proxyCanvas(canvas) {
+        /*
+        {
+            "lineTo": logFunction("CanvasRenderingContext2D.lineTo"),
+            "lineTo": logFunction("CanvasRenderingContext2D.lineTo"),
+        }*/
+        
+        proxyMethod(canvas, "getContext", logFunction("HTMLCanvasElement.getContext", function(arguments, originalFunction) {
+            // contextType, contextAttributes
+            if (arguments[0] == "2d") {
+                if (this._proxyContext2d) return this._proxyContext2d;
+                var context = originalFunction.apply(this, arguments);
+                logAll(context, NAMES.canvasContext2dMethods, "HTMLCanvasElement.CanvasRenderingContext2D.");
+                this._proxyContext2d = context;
+                return context;
+            }
+            else {
+                console.warn("HTMLCanvasElement.getContext: Canvas context type " + arguments[0] + " requested - proxy not implemented");
+            }
+        }));
+    }
+
 })();
